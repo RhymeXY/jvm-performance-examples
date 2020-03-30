435b9afa8e32007f7a27fee7aefbfd63fba24606
RhymeXY <xy_rhyme@163.com>
2020-03-30 22:07:36 +0800

    feat: Add an example of the high usage of IO

--- src/main/java/com/xy/jvmperformanceexamples/controller/Controller.java
+++ src/main/java/com/xy/jvmperformanceexamples/controller/Controller.java
@@ -1,29 +1,55 @@
 package com.xy.jvmperformanceexamples.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.Instant;
 
 /**
  * About JVM performance simulate case.
  *
- * @date 2020年3月29日 19:52:14
  * @author rhyme
+ * @date 2020年3月29日 19:52:14
  */
 @RestController
 public class Controller {
 
     /**
-     * 死循环
+     * 死循环，CPU利用率近乎100%
      */
-    @RequestMapping("/cpu-100")
-    public void endlessLoop(){
-        while (true){
+    @GetMapping("/cpu-100")
+    public void endlessLoop() {
+        while (true) {
 
         }
     }
 
-    @RequestMapping("/hello")
-    public String hello(){
+    /**
+     * 创建1024000个大小为1kb的文件,测试I/O的write
+     */
+    @GetMapping("/io-tough")
+    public void ioTough() throws IOException {
+        final int kb = 1024;
+        final int fileCount = 1024000;
+
+        for (int i = 0; i < fileCount; i++) {
+            String pathname = "/tmp/iotest/" + Instant.now().toEpochMilli() + ".txt";
+            File file = new File(pathname);
+            try (FileOutputStream fileOutputStream = new FileOutputStream(file);) {
+                // 每次输入1 byte，循环1024次
+                for (int j = 0; j < kb; j++) {
+                    fileOutputStream.write(new byte[1]);
+                }
+                fileOutputStream.flush();
+            }
+        }
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
         return "Hello, world!";
     }
 }
